b3169ef7ed21b1b40241a8a0a1b46f52d38d19de
huyuanhao <593746220@qq.com>
2020-06-09 19:02:41 +0800

    推送修改

--- app/src/main/java/com/jime/stu/app/MyApplication.kt
+++ app/src/main/java/com/jime/stu/app/MyApplication.kt
@@ -1,12 +1,15 @@
 package com.jime.stu.app
 
+import android.app.Notification
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import com.aleyn.mvvm.base.BaseApplication
 import com.blankj.utilcode.util.LogUtils
 import com.jime.stu.BuildConfig
 import com.jime.stu.R
 import com.jime.stu.bean.User
+import com.jime.stu.ui.me.MessageActivity
 import com.jime.stu.utils.Preference
 import com.scwang.smartrefresh.layout.SmartRefreshLayout
 import com.scwang.smartrefresh.layout.api.RefreshHeader
@@ -15,6 +18,7 @@ import com.scwang.smartrefresh.layout.header.ClassicsHeader
 import com.umeng.commonsdk.UMConfigure
 import com.umeng.message.IUmengRegisterCallback
 import com.umeng.message.PushAgent
+import com.umeng.message.UmengMessageHandler
 import com.umeng.message.UmengNotificationClickHandler
 import com.umeng.message.entity.UMessage
 import com.umeng.socialize.PlatformConfig
@@ -27,7 +31,8 @@ import kotlin.properties.Delegates
  */
 class MyApplication : BaseApplication() {
     var TAG = "MyApplication"
-    companion object{
+
+    companion object {
         init {
             SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, layout ->
                 layout.setPrimaryColorsId(R.color.colorPrimary, R.color.white);//全局设置主题颜色
@@ -37,9 +42,11 @@ class MyApplication : BaseApplication() {
                 ClassicsFooter(context).setDrawableSize(20f);
             }
         }
+
         var CONTEXT: Context by Delegates.notNull()
         lateinit var USER: User
     }
+
     override fun onCreate() {
         super.onCreate()
         CONTEXT = applicationContext
@@ -50,12 +57,59 @@ class MyApplication : BaseApplication() {
 // 参数三：渠道名称；
 // 参数四：设备类型，必须参数，传参数为UMConfigure.DEVICE_TYPE_PHONE则表示手机；传参数为UMConfigure.DEVICE_TYPE_BOX则表示盒子；默认为手机；
 // 参数五：Push推送业务的secret 填充Umeng Message Secret对应信息（需替换）
-        UMConfigure.init(this, "5ecb8140895cca850100025b", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "e9f97f44a488ab91ab2fb089f62aef0d")
+        UMConfigure.init(
+            this,
+            "5ecb8140895cca850100025b",
+            "Umeng",
+            UMConfigure.DEVICE_TYPE_PHONE,
+            "e9f97f44a488ab91ab2fb089f62aef0d"
+        )
         PlatformConfig.setWeixin("wx3a633c432181b7a8", "279a985b623a404a415d64eac217359f")
         //获取消息推送代理示例
         //获取消息推送代理示例
         val mPushAgent = PushAgent.getInstance(this)
-//注册推送服务，每次调用register方法都会回调该接口
+        val notificationClickHandler: UmengNotificationClickHandler =
+            object : UmengNotificationClickHandler() {
+                override fun dealWithCustomAction(
+                    context: Context,
+                    msg: UMessage
+                ) {
+                    val intent = Intent(context, MessageActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    context.startActivity(intent)
+                }
+            }
+        mPushAgent.notificationClickHandler = notificationClickHandler
+
+        val messageHandler: UmengMessageHandler = object : UmengMessageHandler() {
+            /**
+             * 通知的回调方法（通知送达时会回调）
+             */
+            override fun dealWithNotificationMessage(
+                context: Context?,
+                msg: UMessage
+            ) {
+                LogUtils.e("dealWithNotificationMessage", msg.toString())
+                val json = msg?.raw
+                LogUtils.e("dealWithNotificationMessage", json.toString())
+                //调用super，会展示通知，不调用super，则不展示通知。
+                super.dealWithNotificationMessage(context, msg)
+            }
+//            override fun getNotification(context: Context?, msg: UMessage?): Notification {
+//                LogUtils.e("dealWithNotificationMessage", msg.toString())
+//                val json = msg?.raw
+//                LogUtils.e("dealWithNotificationMessage", json.toString())
+//                return super.getNotification(context, msg)
+//            }
+        }
+//
+        mPushAgent.messageHandler = messageHandler
+        LogUtils.getConfig().run {
+            isLogSwitch = BuildConfig.DEBUG
+            setSingleTagSwitch(true)
+        }
+
+        //注册推送服务，每次调用register方法都会回调该接口
         //注册推送服务，每次调用register方法都会回调该接口
         mPushAgent.register(object : IUmengRegisterCallback {
             override fun onSuccess(deviceToken: String) { //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
@@ -68,26 +122,5 @@ class MyApplication : BaseApplication() {
                 Log.e(TAG, "注册失败：-------->  s:$s,s1:$s1")
             }
         })
-        val notificationClickHandler: UmengNotificationClickHandler =
-            object : UmengNotificationClickHandler() {
-                override fun dealWithCustomAction(
-                    context: Context,
-                    msg: UMessage
-                ) {
-                    LogUtils.e("umessage" + msg.toString())
-                }
-
-                override fun openActivity(context: Context?, msg: UMessage?) {
-                    super.openActivity(context, msg);
-                    LogUtils.e("umessage + openActivity" + msg.toString())
-                }
-            }
-        mPushAgent.notificationClickHandler = notificationClickHandler
-
-        LogUtils.getConfig().run {
-            isLogSwitch = BuildConfig.DEBUG
-            setSingleTagSwitch(true)
-        }
-
     }
 }
